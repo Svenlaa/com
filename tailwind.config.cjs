fb1d8d3c26e8da841bfcc36ead4ad900f30ba760
Svenlaa <sven.lammertink@hotmail.com>
2023-03-24 10:26:16 +0100

    Fix emoji bug

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,6 +1,3 @@
-// eslint-disable-next-line @typescript-eslint/no-var-requires
-const defaultTheme = require("tailwindcss/defaultTheme");
-
 /** @type {import('tailwindcss').Config} */
 
 // eslint-disable-next-line @typescript-eslint/no-var-requires
@@ -15,9 +12,7 @@ module.exports = {
         neutral: colors.slate,
       },
       fontFamily: {
-        sans: ["Noto Color Emoji", ...defaultTheme.fontFamily.sans],
-        serif: ["Noto Color Emoji", ...defaultTheme.fontFamily.serif],
-        mono: ["Noto Color Emoji", ...defaultTheme.fontFamily.mono],
+        emoji: ["Noto Color Emoji"],
       },
     },
   },
