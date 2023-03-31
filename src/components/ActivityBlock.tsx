0343c859f35e031ff8dccd85e5a15a98107b0b55
Svenlaa <sven.lammertink@hotmail.com>
2023-03-31 10:40:01 +0200

    Show multiple colors on Event Week

--- src/components/ActivityBlock.tsx
+++ src/components/ActivityBlock.tsx
@@ -2,17 +2,23 @@ import Link from "next/link";
 import { useRouter } from "next/router";
 
 type ActivityBlockProps = {
-  grade: number;
+  grade: 0 | 1 | 2 | 3 | 4;
   yearWeek: string;
   hasEvent?: boolean;
 };
 
 const ActivityBlock = ({ grade, yearWeek, hasEvent }: ActivityBlockProps) => {
-  let bgColor = "";
-  if (grade === 1) bgColor = "rgb(14, 68, 41)";
-  if (grade === 2) bgColor = "rgb(0, 109, 50)";
-  if (grade === 3) bgColor = "rgb(38, 166, 65)";
-  if (grade === 4) bgColor = "rgb(57, 211, 83)";
+  const gradeColors = [
+    "#0000",
+    "#0e4429",
+    "#006d32",
+    "#26a641",
+    "#39d353",
+  ] as const;
+
+  const firstColor = hasEvent ? "#f59e0b" : gradeColors[grade];
+  const secondColor = !grade && hasEvent ? "#f59e0b" : gradeColors[grade];
+
   const router = useRouter();
 
   const url = `${router.pathname}?filter=${yearWeek}`;
@@ -21,11 +27,10 @@ const ActivityBlock = ({ grade, yearWeek, hasEvent }: ActivityBlockProps) => {
     <Link href={url}>
       <span
         data-aria-grade={grade}
-        className={`flex aspect-square items-center justify-center rounded-sm border-[1px] border-black/30 bg-black/5 p-1 text-center leading-none text-black/75 dark:bg-white/5 dark:text-white/75 ${
-          hasEvent &&
-          "rotate-[96deg] transition-transform duration-700 hover:rotate-[90deg]"
-        }`}
-        style={{ backgroundColor: bgColor || undefined }}
+        className="flex aspect-square items-center justify-center rounded-sm border-[1px] border-black/30 bg-black/5 p-1 text-center leading-none text-black/75 dark:bg-white/5 dark:text-white/75"
+        style={{
+          background: `linear-gradient(135deg, ${firstColor} 0, ${firstColor} 50%, ${secondColor} 50%, ${secondColor} 100%)`,
+        }}
       />
     </Link>
   );
