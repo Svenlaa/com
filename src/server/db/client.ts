f1c6cefe1ea5ae635aea086aa25516cd69b70607
Svenlaa <mail@svenlaa.com>
2023-04-12 20:43:20 +0200

    Kill Prisma!!!

--- src/server/db/client.ts
+++ src/server/db/client.ts
@@ -1,5 +1,4 @@
 // src/server/db/client.ts
-import { PrismaClient } from "@prisma/client";
 import { env } from "../../env/server.mjs";
 import {
   PlanetScaleDatabase,
@@ -9,23 +8,11 @@ import { connect } from "@planetscale/database";
 
 declare global {
   // eslint-disable-next-line no-var
-  var prisma: PrismaClient | undefined;
-
-  // eslint-disable-next-line no-var
   var db: PlanetScaleDatabase | undefined;
 }
-
-export const prisma =
-  global.prisma ||
-  new PrismaClient({
-    log:
-      env.NODE_ENV === "development" ? ["query", "error", "warn"] : ["error"],
-  });
-
 const connection = connect({ url: env.DATABASE_URL });
 export const db = global.db || drizzle(connection);
 
 if (env.NODE_ENV !== "production") {
-  global.prisma = prisma;
   global.db = db;
 }
