66f7a9757177177be2656a9c65e18531bf0c6494
Svenlaa <mail@svenlaa.com>
2023-04-12 17:10:29 +0200

    Update schema

--- src/server/db/schema.ts
+++ src/server/db/schema.ts
@@ -1,9 +1,69 @@
-import { boolean, mediumint, varchar } from "drizzle-orm/mysql-core/columns";
+import {
+  boolean,
+  datetime,
+  int,
+  mediumint,
+  text,
+  varchar,
+} from "drizzle-orm/mysql-core/columns";
+import { uniqueIndex } from "drizzle-orm/mysql-core/indexes";
 import { mysqlTable } from "drizzle-orm/mysql-core/table";
 
 const string = (name: string) => varchar(name, { length: 191 });
 
-export const Run = mysqlTable("Run", {
+export const Example = mysqlTable("example", {
+  id: string("id").primaryKey().notNull(),
+  createdAt: datetime("createdAt", { fsp: 3 }).default(new Date()).notNull(),
+  updatedAt: datetime("updatedAt", { fsp: 3 }).notNull(),
+});
+
+export const Account = mysqlTable(
+  "account",
+  {
+    id: string("id").notNull().primaryKey(),
+    userId: string("userId")
+      .notNull()
+      .references(() => User.id, { onDelete: "cascade", onUpdate: "cascade" }),
+    type: string("type"),
+    provider: string("provider").notNull(),
+    providerAccountId: string("providerAccountId").notNull(),
+    refreshToken: text("refresh_token"),
+    accessToken: text("access_token"),
+    expiresAt: int("expires_at"),
+    tokenType: string("token_type"),
+    scope: string("scope"),
+    idToken: text("id_token"),
+    sessionState: string("session_state"),
+  },
+  (table) => ({
+    Account_provider_providerAccountId_key: uniqueIndex(
+      " Account_provider_providerAccountId_key"
+    ).on(table.provider, table.providerAccountId),
+  })
+);
+
+//TODO Session
+
+//TODO User
+
+export const verificationToken = mysqlTable(
+  "verificationToken",
+  {
+    id: string("identifier").notNull(),
+    token: string("token").notNull(),
+    expires: datetime("expires", { fsp: 3 }).notNull(),
+  },
+  (table) => ({
+    VerificationToken_identifier_token_key: uniqueIndex(
+      "VerificationToken_identifier_token_key"
+    ).on(table.id, table.token),
+    VerificationToken_token_key: uniqueIndex("VerificationToken_token_key").on(
+      table.token
+    ),
+  })
+);
+
+export const Run = mysqlTable("run", {
   id: string("id").primaryKey(),
   distance: mediumint("distance").notNull(),
   time: mediumint("time"),
@@ -12,5 +72,3 @@ export const Run = mysqlTable("Run", {
   isEvent: boolean("isEvent").default(false).notNull(),
   location: string("location"),
 });
-
-//TODO Complete the schema.
