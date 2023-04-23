1c26308ed01e4ba76978ef3e2743c8ce0b9168e1
Svenlaa <mail@svenlaa.com>
2023-04-23 09:28:45 +0200

    Add status

--- src/server/trpc/router/_app.ts
+++ src/server/trpc/router/_app.ts
@@ -1,9 +1,10 @@
 // src/server/trpc/router/_app.ts
-import { router } from "../trpc";
+import { publicProcedure, router } from "../trpc";
 import { runningRouter } from "./running";
 
 export const appRouter = router({
   running: runningRouter,
+  status: publicProcedure.query(() => "up"),
 });
 
 // export type definition of API
